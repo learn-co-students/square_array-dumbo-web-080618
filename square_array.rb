db066002d2f0da7e254e531a4140f8e0fb5413dd
Felix Chan <felixchan29@gmail.com>
2018-07-20 23:47:39 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
 def square_array(array)
   # your code here
+  new_array = []
+  array.each do |number|
+    new_array.push(number ** 2)
+  end
+  new_array
+  
+  #Advance answer using collect
+  #new_array = array.collect { |x| x**2 }
 end
